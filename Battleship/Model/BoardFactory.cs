2b52505d0b2cd646b79ae1edf293d83bfb6ac4f2
mGuy09 <tudor.andreic19@gmail.com>
2022-09-07 10:26:40 +0300

    BoardFactory Square and SquareStatus

--- Battleship/Model/BoardFactory.cs
+++ Battleship/Model/BoardFactory.cs
@@ -2,7 +2,15 @@
 {
     public class BoardFactory
     {
-        
+        public static void ManualBuilder()
+        {
+
+        }
+
+        public static void RandomBuilder()
+        {
+
+        }
     }
 }
 
