672c9ce503943cee04448e40feb9250f7aeab5a6
Madalina Dumitrascu <imadalina.dumitrascu@gmail.com>
2022-09-07 11:33:42 +0300

    Battleship.cs

--- Battleship/Battleship.cs
+++ Battleship/Battleship.cs
@@ -1,12 +1,19 @@
 ﻿using Battleship.Controller;
+using Battleship.Misc;
+using Battleship.View;
 
 namespace Battleship
 {
     public class Battleship
     {
+
         public void Main()
         {
+            bool isGameon = true;
             Game game = new Game();
+            Display.ShowText(Messages.Welcome);
+            Display.ShowText(MainMenu.menu);
+
         }
     }
 } 
